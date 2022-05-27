8f2b59f54f4c80a939096d8fdb69a5587a7a3c9c
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-05-27 22:13:11 +0900

    apply jasypt test_6

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -6,6 +6,9 @@ CURRENT_PORT=$(cat /etc/nginx/conf.d/service_url.inc | grep -Po '[0-9]+' | tail 
 TARGET_PORT=0
 JASYPT_PASSWORD=$(echo $JASYPT_PASSWORD)
 
+# EC2 서버에 미리 만들어둔 jasypt_password를 환경변수에 등록
+#source /etc/profile.d/codedeploy.sh
+
 echo "JASYPT_PASSWORD is ${JASYPT_PASSWORD}"
 echo "> Current port of running WAS is ${CURRENT_PORT}."
 
@@ -25,6 +28,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Pjasypt.encryptor.password=${JASYPT_PASSWORD} -Dserver.port=${TARGET_PORT} /home/ubuntu/eatsring-logging/build/libs/* > /home/ubuntu/eatsring-logging/nohup.out 2>&1 &
+nohup java -jar -DJASYPT_PASSWORD=${JASYPT_PASSWORD} -Dserver.port=${TARGET_PORT} /home/ubuntu/eatsring-logging/build/libs/* > /home/ubuntu/eatsring-logging/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
