ccc7d76445e303c12af0f725d079133a3739f899
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-05-27 17:31:26 +0900

    apply jasypt test_5

--- src/main/java/com/partyluck/party_luck/jasypt/JasyptConfig.java
+++ src/main/java/com/partyluck/party_luck/jasypt/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Value("${JASYPT_PASSWORD}")
+    @Value("${jasypt.encryptor.password}")
     String password;
 
     @Bean(name = "jasyptStringEncryptor")
