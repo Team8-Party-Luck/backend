3ee6e35ae5d5b198b427bad6d4083a156017e74e
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-05-29 00:29:19 +0900

    Update ChatRoom out function2

--- src/main/java/com/partyluck/party_luck/chatmessage/ChatMessageRepository.java
+++ src/main/java/com/partyluck/party_luck/chatmessage/ChatMessageRepository.java
@@ -2,12 +2,11 @@ package com.partyluck.party_luck.chatmessage;
 
 
 
-import com.partyluck.party_luck.chatmessage.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findChatMessagesByChatroom_ChatRoomIdOrderByCreatedAt(String chatroomId);
-    List<ChatMessage> deleteAllByChatroom_ChatRoomId(String chatRoomId);
+    void deleteAllByChatroom_ChatRoomId(String chatRoomId);
 }
