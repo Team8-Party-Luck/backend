44444d8597cc5dda18ca77e91a8f19918b058de0
uchang2001 <uchang2001@hanmail.net>
2022-05-29 00:28:15 +0900

    회원 탈퇴 수정

--- src/main/java/com/partyluck/party_luck/party/repository/PartyRepository.java
+++ src/main/java/com/partyluck/party_luck/party/repository/PartyRepository.java
@@ -4,7 +4,10 @@ import com.partyluck.party_luck.party.domain.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartyRepository extends JpaRepository<Party,Long> {
     void deleteAllByUserid(Long id);
+    List<Party> findAllByUserid(Long id);
 }
